a2deb47c2d1641393c7f881d817873017f925a8b
MattrDizon <matramdizthewirezon.ariesnake@gmail.com>
2023-11-19 22:41:06 +0800

    Server Multi-Client Support
    - Server and Client
    Implemented using sockets and threading. Needs polishing with messages sent and received. Either we use this as a library or implement game logic on server. We'll see soon™️.
    
    - Game State
    Game logic added but incomplete.
    
    Reference:
    https://www.youtube.com/watch?v=xceTFWy_eag

--- client.py
+++ client.py
@@ -1,17 +1,46 @@
+import socket
+import threading
 
-# Import socket module 
-import socket             
- 
-# Create a socket object 
-s = socket.socket()         
- 
-# Define the port on which you want to connect 
-port = 12345               
- 
-# connect to the server on local computer 
-s.connect(('127.0.0.1', port)) 
- 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection 
-s.close() 
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 5566
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+DISCONNECT_MSG = "!DISCONNECT"
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            msg = client_socket.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] {msg}")
+        except Exception as e:
+            print(f"[ERROR] {e}")
+            break
+
+def main():
+    # Client initialization
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+
+    # Handle server incoming msg
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
+
+    connected = True
+    while connected:
+        try:
+            msg = input("> ")
+            client.send(msg.encode(FORMAT))
+        except Exception as e:
+            print(f"[ERROR] {e}")
+            break
+
+        if msg == DISCONNECT_MSG:
+            connected = False
+            
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
