b365bf17e40ac8347fed797dfb4dc60145c5be3e
kuanhh <hannahbenitez62@gmail.com>
2023-11-26 16:58:22 +0800

    Add comments

--- main.py
+++ main.py
@@ -9,14 +9,18 @@ SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Guessing Galore")
 clock = pygame.time.Clock()
 
+
+# Fonts
 font = pygame.font.Font(os.path.join('fonts','InriaSans-Regular.ttf'),20)
 font_italic = pygame.font.Font(os.path.join('fonts','InriaSans-Italic.ttf'),20)
 name_font = pygame.font.Font(os.path.join('fonts','Lalezar-Regular.ttf'),30)
 
+
+# Sounds
 btn_sfx = pygame.mixer.Sound(os.path.join('audio','button_hover.wav'))
 
 
-# Menu assets
+# Graphics
 menu_bg = pygame.image.load(os.path.join('assets','menu_bg.png')).convert_alpha()
 menu_bg_y = 0
 menu_bg_height = menu_bg.get_height()
@@ -44,7 +48,7 @@ mechanics_bg = pygame.image.load(os.path.join('assets','mechanics_bg.png')).conv
 mechanics_bg_rect = mechanics_bg.get_rect(topleft=(26,35))
 
 
-# Timer
+# Timers (custom events)
 bg_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(bg_timer,50)
 
@@ -71,6 +75,7 @@ def draw_mechanics():
         clock.tick(FPS)
 
 
+# Scrolling background effect
 def update_menu_bg():
     global menu_bg_y
     menu_bg_y += 1
@@ -88,6 +93,7 @@ def main_menu():
     
     while True:
         mechanics_clicked = False
+        
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
@@ -98,19 +104,23 @@ def main_menu():
                     mechanics_clicked = True
             if event.type == bg_timer:
                 update_menu_bg()
-                    
+
+        # Background
         SCREEN.fill(('#e1d4bb'))
         SCREEN.blit(menu_bg,(0,menu_bg_y))
         SCREEN.blit(menu_bg,(0,menu_bg_y-menu_bg_height+6))
         
+        # Logo
         SCREEN.blit(logo,logo_rect)
         
+        # Name field
         name_label = font_italic.render('Name', True, (0,0,0))
         name_label_rect = name_label.get_rect(topleft=(325,301))
         SCREEN.blit(name_label, name_label_rect)
         SCREEN.blit(name_field, name_field_rect)
         SCREEN.blit(name_input.surface,(325,332))
         
+        # Buttons
         SCREEN.blit(create_btn,create_btn_rect)
         SCREEN.blit(join_btn,join_btn_rect)
         SCREEN.blit(mechanics_btn,mechanics_btn_rect)
@@ -118,6 +128,7 @@ def main_menu():
         mx, my = pygame.mouse.get_pos()
         lmb_clicked = pygame.mouse.get_pressed()[0]
         
+        # Handle button hover & sfx
         if not field_clicked:
             if create_btn_rect.collidepoint(mx, my):
                 if not create_btn_hovered:
@@ -148,11 +159,12 @@ def main_menu():
                 create_btn_hovered = False
                 join_btn_hovered = False
         
+        # Allow editing of player name if the name field was clicked
         if name_field_rect.collidepoint(mx,my) and lmb_clicked:
             field_clicked = True
         elif field_clicked and lmb_clicked and not name_field_rect.collidepoint(mx,my):
             field_clicked = False
-        # Allow editing of player name if name field was clicked
+        
         if field_clicked: name_input.update(events)
         
         pygame.display.update()
