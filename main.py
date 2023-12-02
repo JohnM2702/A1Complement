528aeb876d15899de2d2e47e6c17d5872b0c6733
kuanhh <hannahbenitez62@gmail.com>
2023-12-02 19:34:42 +0800

    Refactor image loading process

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from asset_loader import load_images
 from pygame_textinput import *
 from network import Network
 from game import Game
@@ -18,82 +19,40 @@ BLUE = '#537188'
 BEIGE = '#E1D4BB'
 
 # Fonts
-font = pygame.font.Font(os.path.join('fonts','InriaSans-Regular.ttf'),20)
-font_italic = pygame.font.Font(os.path.join('fonts','InriaSans-Italic.ttf'),20)
-font_italic_big = pygame.font.Font(os.path.join('fonts','InriaSans-Italic.ttf'),40)
-name_font = pygame.font.Font(os.path.join('fonts','Lalezar-Regular.ttf'),30)
-answer_font = pygame.font.Font(os.path.join('fonts','Lalezar-Regular.ttf'),50)
-game_info_font = pygame.font.Font(os.path.join('fonts','Lalezar-Regular.ttf'),35)
+inria_20 = pygame.font.Font(os.path.join('assets','fonts','InriaSans-Regular.ttf'),20)
+inria_italic_20 = pygame.font.Font(os.path.join('assets','fonts','InriaSans-Italic.ttf'),20)
+inria_italic_40 = pygame.font.Font(os.path.join('assets','fonts','InriaSans-Italic.ttf'),40)
+lalezar_30 = pygame.font.Font(os.path.join('assets','fonts','Lalezar-Regular.ttf'),30)
+lalezar_35 = pygame.font.Font(os.path.join('assets','fonts','Lalezar-Regular.ttf'),35)
+lalezar_50 = pygame.font.Font(os.path.join('assets','fonts','Lalezar-Regular.ttf'),50)
 
 # Player Name
 manager = TextInputManager(validator=lambda input: len(input) <= 15)
-name_input = TextInputVisualizer(manager,name_font,cursor_blink_interval=750,cursor_width=2)
+name_input = TextInputVisualizer(manager,lalezar_30,cursor_blink_interval=750,cursor_width=2)
 
 # Sounds
-btn_sfx = pygame.mixer.Sound(os.path.join('audio','button_hover.wav'))
+btn_sfx = pygame.mixer.Sound(os.path.join('assets','sfx','button_hover.wav'))
 
+# Images
+images: dict[str,pygame.Surface] = load_images()
 
-# Graphics
-menu_bg = pygame.image.load(os.path.join('assets','menu_bg.png')).convert_alpha()
+logo_rect = images['logo'].get_rect(topleft = (280,46))
+name_field_rect = images['name_field'].get_rect(topleft = (292,325))
+create_btn_rect = images['create_btn'].get_rect(topleft = (355,417))
+join_btn_rect = images['join_btn'].get_rect(topleft = (355,525))
+mechanics_btn_rect = images['mechanics_btn'].get_rect(topleft = (355,633))
+mechanics_bg_rect = images['mechanics_bg'].get_rect(topleft=(26,35))
+number_two_rect = images['number_two'].get_rect(topleft=(301,522))
+number_four_rect = images['number_four'].get_rect(topleft=(599,522))
+number_three_rect = images['number_three'].get_rect(topleft=(447,522))
+loading_bg_rect = images['loading_bg'].get_rect(topleft=(269,384))
+enter_btn_rect = images['enter_btn'].get_rect(topleft=(401,570))
+name_box_rect = images['name_box'].get_rect(topleft=(355,487))
+
+pcard_width, pcard_height = images['player_card'].get_size()
+gbox_width, gbox_height = images['game_box'].get_size()
+bg_height = images['menu_bg'].get_height()
 bg_y = 0
-bg_height = menu_bg.get_height()
-game_bg = pygame.image.load(os.path.join('assets','game_bg.png')).convert_alpha()
-
-logo = pygame.image.load(os.path.join('assets','logo.png')).convert_alpha()
-logo_rect = logo.get_rect(topleft = (280,46))
-
-name_field = pygame.image.load(os.path.join('assets','name_field.png')).convert_alpha()
-name_field_rect = name_field.get_rect(topleft = (292,325))
-
-create_btn = pygame.image.load(os.path.join('assets','create.png')).convert_alpha()
-create_btn_hover = pygame.image.load(os.path.join('assets','create_hover.png')).convert_alpha()
-create_btn_rect = create_btn.get_rect(topleft = (355,417))
-
-join_btn = pygame.image.load(os.path.join('assets','join.png')).convert_alpha()
-join_btn_hover = pygame.image.load(os.path.join('assets','join_hover.png')).convert_alpha()
-join_btn_rect = join_btn.get_rect(topleft = (355,525))
-
-loading0_icon = pygame.image.load(os.path.join('assets','loading_0.png')).convert_alpha()
-loading1_icon = pygame.image.load(os.path.join('assets','loading_1.png')).convert_alpha()
-
-mechanics_btn = pygame.image.load(os.path.join('assets','mechanics.png')).convert_alpha()
-mechanics_btn_hover = pygame.image.load(os.path.join('assets','mechanics_hover.png')).convert_alpha()
-mechanics_btn_rect = mechanics_btn.get_rect(topleft = (355,633))
-
-mechanics_bg = pygame.image.load(os.path.join('assets','mechanics_bg.png')).convert_alpha()
-mechanics_bg_rect = mechanics_bg.get_rect(topleft=(26,35))
-
-number_two = pygame.image.load(os.path.join('assets','number_two.png')).convert_alpha()
-number_two_hover = pygame.image.load(os.path.join('assets','number_two_hover.png')).convert_alpha()
-number_two_rect = number_two.get_rect(topleft=(301,522))
-number_four = pygame.image.load(os.path.join('assets','number_four.png')).convert_alpha()
-number_four_hover = pygame.image.load(os.path.join('assets','number_four_hover.png')).convert_alpha()
-number_four_rect = number_four.get_rect(topleft=(599,522))
-number_three = pygame.image.load(os.path.join('assets','number_three.png')).convert_alpha()
-number_three_hover = pygame.image.load(os.path.join('assets','number_three_hover.png')).convert_alpha()
-number_three_rect = number_three.get_rect(topleft=(447,522))
-
-loading_bg = pygame.image.load(os.path.join('assets','loading.png')).convert_alpha()
-loading_bg_rect = loading_bg.get_rect(topleft=(269,384))
-
-enter_btn = pygame.image.load(os.path.join('assets','enter.png')).convert_alpha()
-enter_btn_hover = pygame.image.load(os.path.join('assets','enter_hover.png')).convert_alpha()
-enter_btn_rect = enter_btn.get_rect(topleft=(401,570))
-
-name_box = pygame.image.load(os.path.join('assets','name_box.png')).convert_alpha()
-name_box_rect = name_box.get_rect(topleft=(355,487))
-
-question_box = pygame.image.load(os.path.join('assets','question_box.png')).convert_alpha()
-answer_box = pygame.image.load(os.path.join('assets','answer_box.png')).convert_alpha()
-player_card = pygame.image.load(os.path.join('assets','player_card.png')).convert_alpha()
-pcard_width, pcard_height = player_card.get_size()
-
-game_box = pygame.image.load(os.path.join('assets','game_box.png')).convert_alpha()
-gbox_width, gbox_height = game_box.get_size()
-game_box_hover = pygame.image.load(os.path.join('assets','game_box_hover.png')).convert_alpha()
-circle_waiting = pygame.image.load(os.path.join('assets','circle_waiting.png')).convert_alpha()
-circle_full = pygame.image.load(os.path.join('assets','circle_full.png')).convert_alpha()
-
 
 # Global Timers (custom events)
 bg_timer = pygame.USEREVENT + 1
@@ -103,14 +62,14 @@ round_timer = pygame.USEREVENT + 3
 pygame.time.set_timer(bg_timer,50)
 
 
-def draw_bg(surface=None,rect=None,bg=menu_bg,draw_logo=True,color=BEIGE):
+def draw_bg(surface=None,rect=None,bg=images['menu_bg'],draw_logo=True,color=BEIGE):
     SCREEN.fill((color))
     SCREEN.blit(bg,(0,bg_y))
     SCREEN.blit(bg,(0,bg_y-bg_height+6))
     if surface is not None and rect is not None:
         SCREEN.blit(surface,rect)
     if draw_logo:
-        SCREEN.blit(logo,logo_rect)
+        SCREEN.blit(images['logo'],logo_rect)
 
 
 def mechanics():
@@ -125,15 +84,15 @@ def mechanics():
                     running = False
             if event.type == bg_timer:
                 scroll_bg()
-        draw_bg(mechanics_bg,mechanics_bg_rect)
+        draw_bg(images['mechanics_bg'],mechanics_bg_rect)
         pygame.display.update()
         clock.tick(FPS)
         
 
 def define_player_window():
-    btn_rects = [[number_two,number_two_hover,number_two_rect,False,2],
-                     [number_three,number_three_hover,number_three_rect,False,3],
-                     [number_four,number_four_hover,number_four_rect,False,4]]
+    btn_rects = [[images['number_two'],images['number_two_hover'],number_two_rect,False,2],
+                     [images['number_three'],images['number_three_hover'],number_three_rect,False,3],
+                     [images['number_four'],images['number_four_hover'],number_four_rect,False,4]]
     running = True
     while running:
         lmb_clicked = False
@@ -150,9 +109,9 @@ def define_player_window():
             if event.type == bg_timer:
                 scroll_bg()
                 
-        draw_bg(loading_bg,loading_bg_rect)
+        draw_bg(images['loading_bg'],loading_bg_rect)
 
-        waiting_label = font_italic_big.render("Chose the Player Size",1,'Black')
+        waiting_label = inria_italic_40.render("Chose the Player Size",1,'Black')
         SCREEN.blit(waiting_label,(332,419))
 
         mx, my = pygame.mouse.get_pos()
@@ -218,23 +177,23 @@ def loading(game: Game):
                             break
 
         # Draw the screen
-        draw_bg(loading_bg, loading_bg_rect)
+        draw_bg(images['loading_bg'], loading_bg_rect)
 
         game_id = game.get_id()
-        game_number = game_info_font.render(f'Game {game_id}', 1, 'black')
+        game_number = lalezar_35.render(f'Game {game_id}', 1, 'black')
         SCREEN.blit(game_number, (449, 390))
 
-        waiting_label = font_italic_big.render('Waiting for Players', 1, 'black')
+        waiting_label = inria_italic_40.render('Waiting for Players', 1, 'black')
         SCREEN.blit(waiting_label, (347, 444))
 
         player_count = game.get_player_count()
         player_size = game.get_player_size()
         player_count_text = f'{player_count}/{player_size}'
-        player_count_render = font_italic_big.render(player_count_text, 1, 'Black')
+        player_count_render = inria_italic_40.render(player_count_text, 1, 'Black')
         SCREEN.blit(player_count_render, (483, 500))
 
         for i in range(0, len(loading_array)):
-            selected_icon = loading0_icon if loading_array[i] == 0 else loading1_icon
+            selected_icon = images['loading_0'] if loading_array[i] == 0 else images['loading_1']
             SCREEN.blit(selected_icon, (358 + (67 * i), 576))
 
         pygame.display.update()
@@ -284,22 +243,22 @@ def view_games():
             if event.type == bg_timer:
                 scroll_bg()
         
-        draw_bg(mechanics_bg,mechanics_bg_rect)
+        draw_bg(images['mechanics_bg'],mechanics_bg_rect)
         games = fetch_games()
 
         if isinstance(games, dict) and len(games) > 0:
             game_box_rects = []
             count, x, y = 1, 0, 340
-            circle = circle_waiting
+            circle = images['circle_waiting']
 
             for game_id, game in games.items():
-                game_number = game_info_font.render(f'Game {game_id}',1,'black')
+                game_number = lalezar_35.render(f'Game {game_id}',1,'black')
                 p_count = game.get_player_count()
                 p_size = game.get_player_size()
                 player_count_text = f'{p_count}/{p_size}'
-                player_count_surf = game_info_font.render(player_count_text,1,'Black')
+                player_count_surf = lalezar_35.render(player_count_text,1,'Black')
                 
-                if p_count == p_size: circle = circle_full
+                if p_count == p_size: circle = images['circle_full']
                 
                 if count % 2 == 0: x = 586
                 else: x = 121
@@ -319,11 +278,11 @@ def view_games():
             # Handle button hover, sfx, and click
             for rect in game_box_rects:
                 if rect[0].collidepoint(mx,my): 
-                    SCREEN.blit(game_box_hover,rect[0])
+                    SCREEN.blit(images['game_box_hover'],rect[0])
                     if lmb_clicked:
                         join_game(rect[4])
                 else: 
-                    SCREEN.blit(game_box,rect[0])
+                    SCREEN.blit(images['game_box'],rect[0])
                 SCREEN.blit(circle,rect[1])
                 SCREEN.blit(game_number,rect[2])
                 SCREEN.blit(player_count_surf,rect[3])
@@ -355,7 +314,7 @@ def game_proper(game:Game):
     # Temporary max input length
     # ideal: dynamically set when client can receive answers from server
     manager = None
-    answer_input = TextInputVisualizer(manager,answer_font,cursor_blink_interval=500,cursor_width=0)
+    answer_input = TextInputVisualizer(manager,lalezar_50,cursor_blink_interval=500,cursor_width=0)
     ongoing = True
     
     while ongoing:
@@ -385,22 +344,22 @@ def game_proper(game:Game):
         
 
 def draw_game(game:Game):
-    draw_bg(bg=game_bg,draw_logo=False,color=BLUE)
-    SCREEN.blit(question_box,(23,18))
+    draw_bg(bg=images['game_bg'],draw_logo=False,color=BLUE)
+    SCREEN.blit(images['question_box'],(23,18))
     players = game.get_players()
     card_x, card_y = 23, 494
     name_x, name_y = 35, 562
     score_x, score_y = 35, 602
     for id, data in players.items():
-        name = name_font.render(data['name'],1,'black')
-        score = name_font.render(str(data['score']),1,'black')
-        SCREEN.blit(player_card,(card_x,card_y))
+        name = lalezar_30.render(data['name'],1,'black')
+        score = lalezar_30.render(str(data['score']),1,'black')
+        SCREEN.blit(images['player_card'],(card_x,card_y))
         SCREEN.blit(name,(name_x,name_y))
         SCREEN.blit(score,(score_x,score_y))
         card_x += 248
         name_x += 248
         score_x += 248
-    SCREEN.blit(answer_box,(23,669))
+    SCREEN.blit(images['answer_box'],(23,669))
 
 
 # Scrolling background effect
@@ -414,9 +373,9 @@ def scroll_bg():
 def main_menu():
     field_clicked = False
     name_input.cursor_visible = False
-    btn_rects = [[create_btn,create_btn_hover,create_btn_rect,False],
-                     [join_btn,join_btn_hover,join_btn_rect,False],
-                     [mechanics_btn,mechanics_btn_hover,mechanics_btn_rect,False]]
+    btn_rects = [[images['create_btn'],images['create_btn_hover'],create_btn_rect,False],
+                     [images['join_btn'],images['join_btn_hover'],join_btn_rect,False],
+                     [images['mechanics_btn'],images['mechanics_btn_hover'],mechanics_btn_rect,False]]
     
     while True:
         lmb_clicked = False
@@ -435,10 +394,10 @@ def main_menu():
         draw_bg()
         
         # Name field
-        name_label = font_italic.render('Hello there,', True, (0,0,0))
+        name_label = inria_italic_20.render('Hello there,', True, (0,0,0))
         name_label_rect = name_label.get_rect(topleft=(325,301))
         SCREEN.blit(name_label, name_label_rect)
-        SCREEN.blit(name_field, name_field_rect)
+        SCREEN.blit(images['name_field'], name_field_rect)
         SCREEN.blit(name_input.surface,(325,332))
         
         mx, my = pygame.mouse.get_pos()
@@ -456,9 +415,9 @@ def main_menu():
                                 other_rect[3] = False 
                     SCREEN.blit(rect[1],rect[2])
                     if lmb_clicked:
-                        if rect[0] is create_btn:
+                        if rect[0] is images['create_btn']:
                             define_player_window()
-                        elif rect[0] is join_btn:
+                        elif rect[0] is images['join_btn']:
                             view_games()
                         else: 
                             mechanics()
@@ -494,15 +453,15 @@ def player_name():
             if event.type == bg_timer:
                 scroll_bg()
 
-        draw_bg(loading_bg,loading_bg_rect)
+        draw_bg(images['loading_bg'],loading_bg_rect)
 
         # Label
-        window_label = font_italic_big.render("Enter Your Name",1,'Black')
+        window_label = inria_italic_40.render("Enter Your Name",1,'Black')
         SCREEN.blit(window_label,(365,419))
         
         # Namebox and Button
-        SCREEN.blit(name_box,name_box_rect)
-        SCREEN.blit(enter_btn,enter_btn_rect)
+        SCREEN.blit(images['name_box'],name_box_rect)
+        SCREEN.blit(images['enter_btn'],enter_btn_rect)
 
         # Get the value of name_input
         player_name_value = name_input.value
@@ -521,7 +480,7 @@ def player_name():
                 if not enter_btn_hovered:
                     btn_sfx.play()
                     enter_btn_hovered = True
-                SCREEN.blit(enter_btn_hover, enter_btn_rect)
+                SCREEN.blit(images['enter_btn_hover'], enter_btn_rect)
                 if lmb_clicked:
                     print(player_name_value,"has opened the game")
                     while True:
