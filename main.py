71cefc802f117275ac9f65f3fde7e1f4c9682aa8
MattrDizon <matramdizthewirezon.ariesnake@gmail.com>
2023-12-06 06:38:07 +0800

    QoL import order
    As requested by @hlowrain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+from network import Network, net_test
 from asset_loader import load_images
 from pygame_textinput import *
 from threading import Thread
-from network import Network, net_test
 from game import Game
 from sys import exit
 import pygame, os
