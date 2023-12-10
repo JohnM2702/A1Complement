5f34701498cc8311683e99e56eefd20924063376
thekatisdead <kaherbieto@up.edu.ph>
2023-12-11 02:44:19 +0800

    added questions and fixed readme for release

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# A1Complement
-LAN-based multiplayer game
+# Guessing Galore
+
+Are you ready to sharpen your general knowledge skills? Or Maybe you are looking to play a fun game of trivia with your friends or family, or maybe with both?
+
+Guessing Galore is a trivia game that allows multiple computers in a Local Area Network (LAN) to join and play the game. That way, as long as you have a computer that has the game, you can join in and play trivia with people around you.
+
+## Prerequisites
+In order to play the game, you need to make sure that the following are installed in your computer:
+
+    1. Python 3  
+        a. Pygame Community  
+        b. Pygame Text Input Module  
+        c. Dill
+
+If you don’t have Python 3 installed in your computer, you can download Python 3 by going to their website www.python.org  
+
+Once you have Python 3 installed, input the following commands to the command prompt to have the modules mentioned installed to your Python 3:
+
+    pip install pygame-ce
+    pip install pygame-textinput
+    pip install dill
+
+Once you have all the prerequisites accomplished, you are now ready to play our game! 😁
+
+## How to Run the Game
+**I. Create a Server**  
+Before you get to play the game, we need to make sure that a server exists in our network. If no server exists in the network, then the game cannot run. To do this, just run the
+
+    server.py
+
+**II. Open a Game**  
+
+When a server exists in our network, we should be able to open the game without any complications. To do this, open main.py which will lead you a screen in where you will input the server ip and your name. *Server ip* is found when someone accomplished ***Creating a Server***
+
+**III. Creating a Game**  
+You can create a game by clicking the “Create Game” button on the Main Menu and defining how many people that will be the game and the category of the game.
+
+**IV. Joining a Game**  
+If one of your companions have created a game, you can simply join their game by clicking the “Join Game” button and clicking their room ID.
+
+**V. Game Proper**  
+This game is played in a group (2 - 4 players). Each player is given the same set of words to guess, but the words are arranged randomly. Players have a set amount of time to answer before the round will finish. The faster you guess, the more points you have and the more points you have, the more knowledgeable you are  
+
+After ten (10) rounds, the game ends and the player with the highest score wins the game.
+
+
+## Acknowledgements
+***Guessing Galore Game***  
+Created by: A1 Complement
+
+**Project Manager**  
+Matthew Dizon
+
+**Backend Developers**
+Matthew Dizon  
+John Mynar Laquinta  
+Junry Emanuel Mingo  
+
+**Frontend Developers**  
+Hannah Lorraine Benitez  
+Kermichil Herbieto  
+Hazelle Anne Milan
+
--- q_networking.py
+++ q_networking.py
@@ -1,1 +1,226 @@
-networking_qna = [['The IETF standards documents are called ________:', 'RFC'], ['In the layer hierarchy as the data packet moves from the upper to the lower layers, headers are ___________ ', 'Added'], ['A _______ is the physical path over which a message travels.', 'Medium'], ['Which of this is not a network edge device?', 'Switches'], ['A _________ set of rules that governs data communication.', 'Protocols'], ['In computer network nodes are _________ ', 'all of the mentioned'], ['A __________ is a device that forwards packets between networks by processing the routing information included in the packet.', 'router'], ['Network congestion occurs _________ ', 'in case of traffic overloading'], ['HFC contains _______ ', 'A combination of Fibre cable and Coaxial cable'], ['How many layers are present in the Internet protocol stack (TCP/IP model)?', '5'], ['The number of layers in ISO OSI reference model is __________ ', '7'], ['Which of the following layers is an addition to OSI model when compared with TCP IP model?', 'Session and Presentation layer'], ['Application layer is implemented in ____________ ', 'End system'], ['Transport layer is implemented in ______________', 'End system'], ['The functionalities of the presentation layer include ____________', 'All of the mentioned'], ['Delimiting and synchronization of data exchange is provided by __________ ', 'Session layer'], ['In OSI model, when data is sent from device A to device B, the 5th layer to receive data at B is _________  ', 'Session layer'], ['In TCP IP Model, when data is sent from device A to device B, the 5th layer to receive data at B is ____________ ', 'Application layer'], ['In the OSI model, as a data packet moves from the lower to the upper layers, headers are _______ ', 'Removed'], ['OSI stands for __________', 'open system interconnection'], ['Which layer is responsible for process to process delivery in a general network model? ', 'transport layer'], ['Which address is used to identify a process on a host by the transport layer?', 'port address'], ['Transmission data rate is decided by ____________ ', 'physical layer'], ['Which is not a application layer protocol?', 'TCP'], ['The packet of information at the application layer is called __________ ', 'Message'], ['Which one of the following is an architecture paradigm?', 'Both Peer-to-Peer & Client-Server'], ['Application layer offers _______ service.', 'End to end'], ['E-mail is _________', 'Elastic application'], ['Which of the following is an application layer service?', 'All of the mentioned'], ['To deliver a message to the correct application program running on a host, the _______ address must be consulted.', 'Port'], ['Electronic mail uses which Application layer protocol?', 'SMTP'], ['The ____________ translates internet domain and host names to IP address.', 'domain name system'], ['Application layer protocol defines ____________', 'all of the mentioned'], ['Which one of the following protocol delivers/stores mail to reciever server?', 'simple mail transfer protocol'], ['Which one of the following is an internet standard protocol for managing devices on IP network?', 'simple network management protocol'], ['Which one of the following is not an application layer protocol?', 'resource reservation protocol'], ['Which one of the following is not correct?', 'All of the mentioned'], ['When displaying a web page, the application layer uses the _____________', 'HTTP protocol'], ['A DNS client is called _________ ', 'DNS resolver'], ['DNS database contains _______ ', 'all of the mentioned'], ['If a server has no clue about where to find the address for a hostname then _______ ', 'server asks to the root server'], ['Which one of the following allows client to update their DNS entry as their IP address change?', 'dynamic DNS'], ['The right to use a domain name is delegated by domain name registers which are accredited by _______ ', 'internet corporation for assigned names and numbers'], ['The domain name system is maintained by _______', 'distributed database system'], ['The time taken by a packet to travel from client to server and then back to the client is called __________ ', 'RTT'], ['The HTTP request message is sent in _________ part of three-way handshake.', 'Third'], ['In the process of fetching a web page from a server the HTTP request/response takes __________ RTTs.', '1'], ['The first line of HTTP request message is called _____________ ', 'Request line'], ['The values GET, POST, HEAD etc are specified in ____________ of HTTP message', 'Request line'], ['The __________ method when used in the method field, leaves entity body empty.', 'GET'], ['The conditional GET mechanism ', 'Helps to keep a cache upto date'], ['HTTP is ________ protocol.', 'application layer'], ['In the network HTTP resources are located by ', 'uniform resource identifier'], ['FTP server listens for connection on port number ____________ ', '21'], ['Expansion of FTP is __________ ', 'File Transfer Protocol'], ['FTP is built on _____ architecture.', 'Client-server'], ['The password is sent to the server using ________ command ', 'PASS'], ['Expansion of SMTP is ________ ', 'Simple Mail Transfer Protocol'], ['User agent does not support this ___________ ', 'Routing messages'], ['Which of the following is false with respect to TCP?', 'Unreliable'], ['In TCP, sending and receiving data is done as _______ ', 'Stream of bytes'], ['To achieve reliable transport in TCP, ___________ is used to check the safe and sound arrival of data.', 'Acknowledgment'], ['The receiver of the data controls the amount of data that are to be sent by the sender is referred to as ___________', 'Flow control'], ['Connection establishment in TCP is done by which mechanism? ', 'Three-Way Handshaking'], ['The sizes of source and destination port address in TCP header are ___________ respectively.', '16-bits and 16-bits'], ['Which of the following is false with respect to UDP?', 'Connection-oriented'], ['What is the main advantage of UDP?', 'Low overhead'], ['What is the header size of a UDP packet?', '8 bytes'], ['The term that is used to place packet in its route to its destination is called __________ ', 'Forwarding'], ['What is WPA?', 'wi-fi protected access'], ['What is internet?', 'a vast collection of different networks'], ['To join the internet, the computer has to be connected to a _________ ', 'internet service provider'], ['The size of an IP address in IPv6 is _________ ', '128 bits'], ['Internet works on _______ ', 'packet switching'], ['Multiple processes on destinations at transport layer are identified by __________', 'Port number'], ['Range of port numbers in Internet model is __________', '0 and 65,535(16-bit)'], ['The combination of an IP address and port number is called as ________ ', 'Socket address'], ['Which of the following is false with respect to Connectionless service of transport layer protocol?', 'Packets are not delayed'], ['URL stands for ________ ', 'uniform resource locator']]
+networking_qna = [
+    [
+        'The IETF standards documents are called ________:', 
+        'RFC'
+    ], 
+    [
+        'In the layer hierarchy as the data packet moves from the upper to the lower layers, headers are ___________ ', 
+        'Added'
+    ], 
+    [
+        'The physical path over which a message travels.', 
+        'Medium'
+    ],
+    [
+        'A _________ set of rules that governs data communication.', 
+        'Protocols'
+    ],
+    [
+        'A __________ is a device that forwards packets between networks by processing the routing information included in the packet.', 
+        'router'
+    ], 
+    [
+        'How many layers are present in the Internet protocol stack (TCP/IP model)?',
+        '5'
+    ],
+    [
+        'The number of layers in ISO OSI reference model is __________ ', 
+        '7'
+    ],
+    [
+        'Application layer is implemented in ____________ ',
+        'End system'
+    ],
+    [
+        'Transport layer is implemented in ______________',
+        'End system'
+    ],
+    [
+        'Delimiting and synchronization of data exchange is provided by __________ ',
+        'Session layer'
+    ],
+    [
+        'In OSI model, when data is sent from device A to device B, the 5th layer to receive data at B is _________  ',
+        'Session layer'
+    ],
+    [
+        'In TCP IP Model, when data is sent from device A to device B, the 5th layer to receive data at B is ____________ ',
+        'Application layer'
+    ],
+    [
+        'In the OSI model, as a data packet moves from the lower to the upper layers, headers are _______ ',
+        'Removed'
+    ],
+    [
+        'What address is used to identify a process on a host by the transport layer?',
+        'port address'
+    ],
+    [
+        'Transmission data rate is decided by ____________ ',
+        'physical layer'
+    ],
+    [
+        'The packet of information at the application layer is called __________ ',
+        'Message'
+    ],
+    [
+        'Application layer offers _______ service.',
+        'End to end'
+    ],
+    [
+        'E-mail is _________',
+        'Elastic application'
+    ],
+    [
+        'To deliver a message to the correct application program running on a host, the _______ address must be consulted.',
+        'Port'
+    ], 
+    [
+        'Electronic mail uses what Application layer protocol?',
+        'SMTP'
+    ],
+    [
+        'The ____________ translates internet domain and host names to IP address.',
+        'domain name system'
+    ],
+    [
+        'When displaying a web page, the application layer uses the _____________',
+        'HTTP protocol'
+    ],
+    [
+        'The values GET, POST, HEAD etc are specified in ____________ of HTTP message',
+        'Request line'
+    ],
+    [
+        'HTTP is ________ protocol.',
+        'application layer'
+    ],
+    [
+        'FTP is built on _____ architecture.',
+        'Client-server'
+    ],
+    [
+        'The password is sent to the server using ________ command ',
+        'PASS'
+    ],
+    [
+        'To achieve reliable transport in TCP, ___________ is used to check the safe and sound arrival of data.',
+        'Acknowledgment'
+    ],
+    [
+        'The receiver of the data controls the amount of data that are to be sent by the sender is referred to as ___________',
+        'Flow control'
+    ],
+    [
+        'The sizes of source and destination port address in TCP header are ___________ respectively.',
+        '16-bits and 16-bits'
+    ],
+    [
+        'What is the header size of a UDP packet?',
+        '8 bytes'
+    ],
+    [
+        'The term that is used to place packet in its route to its destination is called __________ ',
+        'Forwarding'
+    ],
+    [
+        'The size of an IP address in IPv6 is _________ ',
+        '128 bits'
+    ],
+    [
+        'Internet works on _______ ',
+        'packet switching'
+    ],
+    [
+        'Multiple processes on destinations at transport layer are identified by __________',
+        'Port number'
+    ],
+    [
+        'Range of port numbers in Internet model is __________',
+        '0 and 65,535(16-bit)'
+    ],
+    [
+        'The combination of an IP address and port number is called as ________ ',
+        'Socket address'
+    ],
+    [
+        'URL stands for ________ ',
+        'uniform resource locator'
+    ],
+    [
+        'It is the active process of transporting data from one point to another',
+        'Data Communication'
+    ],
+    [
+        'Refers to the variation in packet arrival time in Effective Data Communication',
+        'Jitter'
+    ],
+    [
+        'A type of Data Flow where one sends and the other only receives',
+        'Simplex'
+    ],
+    [
+        'The measurement of communication data transfer rate',
+        'Baud'
+    ],
+    [
+        'a device that modulates an analog carrier signal to encode digital information, and also demodulates such a carrier signal',
+        'modem'
+    ],
+    [
+        'The interconnection of various computer systems located at different places',
+        'Computer Network'
+    ],
+    [
+        'A Fully connected _____ has every device connected point-to-point to every other device',
+        'mesh'
+    ],
+    [
+        'The ____ Topology passes its signal in one direction until it reaches its destination',
+        'ring'
+    ]
+    [
+        'The ______ defines the procedures and functions that physical devices and interfaces have to perform for transmission to occur',
+        'Physical Layer'
+    ],
+    [
+        'The range of frequencies contained in a composite signal',
+        'Bandwidth'
+    ],
+    [
+        'A type of data flow where both devices can transmit and receive information, but not at the same time',
+        'half-duplex'
+    ],
+    [
+        'A type of data flow where both devices can transmit and receive information',
+        'full-duplex'
+    ],
+    [
+        'It is the set of techniques that allows the simultaneous transmission of multiple signals across a single data link',
+        'Multiplexing'
+    ]
+    [
+        'A type of noise where one wire affects another wire',
+        'crosstalk'
+    ],
+    [
+        'A type of transmission impairment when the signal changes shape',
+        'distortion'
+    ],
+    [
+        'Created by calculating the binary values in a packet or other block of data using some algorithm',
+        'Checksum'
+    ],
+    [
+        'This allows you to determine the start and end of a packet',
+        'Frame'
+    ],
+    [
+        'The __________ sends one frame, stops until it receives confirmation that the receiver receives the frame before sending a new message',
+        'Stop-and-wait'
+    ],
+    [
+        'The layer responsible for the process-to-process delivery of the message',
+        'Transport Layer'
+    ]
+]
